f7ed2605b815cfb10d1cdc44242059834ee442bd
raravind15 <raravind15@gmail.com>
2026-05-03 18:29:59 +0530

    Added validators.py for validating file and data

--- src/file_utils.py
+++ src/file_utils.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from google.cloud import storage
 import re
 from config import CONFIG
-
+import tempfile
+import os
 logging.basicConfig(level=logging.INFO,format=f"%(asctime)s-%(levelname)s-%(message)s")
 logger=logging.getLogger(__name__)
 
@@ -23,6 +24,24 @@ def list_files(project_id,bucket_name,prefix):
     
     return file_names
 
-files=list_files(CONFIG["project_id"],CONFIG["landing_bucket"],CONFIG["landing_prefix"])
 
-print(files)
+def download_file(project_id,bucket_name,file_name):
+    client=get_gcs_client(project_id)
+    bucket=client.bucket(bucket_name)
+    file_blob=bucket.blob(file_name)
+    temp_file=tempfile.NamedTemporaryFile(delete=False,suffix=".parquet")
+    file_blob.download_to_file(temp_file)
+    temp_file.close()
+    return temp_file.name
+
+def move_file(project_id,source_bucket,target_bucket,source_file,prefix):
+    client=get_gcs_client(project_id)
+    src_bkt=client.bucket(source_bucket)
+    tgt_bkt=client.bucket(target_bucket)
+    src_blob=src_bkt.blob(source_file)
+    tgt_file=f"{prefix}/{source_file.split("/")[-1]}"
+    tgt_blob=tgt_bkt.blob(tgt_file)
+    tgt_blob.rewrite(src_blob)
+    src_blob.delete()
+
+
--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 import file_utils as file_utils
+import validators
+from config import CONFIG
+import logging
+
+logger=logging.getLogger("__name__")
 
 def main():
     print("Started Process")
+    try:
+        file_name,row_count=validators.file_validation(CONFIG["project_id"],CONFIG["landing_bucket"],CONFIG["landing_prefix"])
+        validators.data_validation(CONFIG["project_id"],CONFIG["landing_bucket"],file_name,CONFIG["mandatory_columns"],CONFIG["mandatory_non_null_columns"])
+    except Exception as e:
+        error_message=str(e)
+        logger.error(f"error:{error_message}")
 
-if __name__==main():
+
+if __name__=="__main__":
     main()
